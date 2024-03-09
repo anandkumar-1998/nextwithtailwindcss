a84ab61e6a3fbfdf95a5b19a85beddebb84583da
anand kumar <anand.kumar@nidester.com>
2024-03-09 19:31:41 +0530

    ui: sidebar

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="border-[1px] border-black p-2 bg-white text-black">
+    tailwind css with next 14
+    </div>
   );
 }
